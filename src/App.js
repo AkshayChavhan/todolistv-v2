559976adad84731ffb45e9cff2c523a4468bf8c1
Akshay Chavhan <86603141+AkshayChavhan@users.noreply.github.com>
2022-03-26 15:32:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Todo from './Components/Todo'
+import './App.css';
+import ShowTodo from './Components/ShowTodo'
+
+function App() {
+  return (
+    <div className="App">
+     <Todo/>
+     {/* <ShowTodo /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/ShowTodo.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function ShowTodo(props) {
+    return (
+        <div className='container'>
+            <div className='row my-2'>
+                <div className='col-6'>
+                    <h6>{props.task}</h6>
+                </div>
+                <div className='col-6'>
+                    <button onClick={()=>{
+                        props.onSelect(props.id)
+                    }}>X</button>
+                </div>
+            </div>
+
+        </div>
+    )
+}
